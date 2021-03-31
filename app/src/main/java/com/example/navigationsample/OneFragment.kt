dea15fc02ea9558869f7582d9efe25d8df847cab
pisuma <satojota@MacBook-Pro-2.local>
2021-03-31 16:25:34 +0900

    Navigatioライブラリを用いた画面遷移完了

---
+++ app/src/main/java/com/example/navigationsample/OneFragment.kt
@@ -0,0 +1,32 @@
+package com.example.navigationsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.one_button)
+        button.setOnClickListener {
+            val navController = view.findNavController()
+            navController.navigate(R.id.action_oneFragment_to_secondFragment)
+        }
+
+    }
+}
